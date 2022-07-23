707ec316e18a96b6eebc93fea626eb72d0705b0d
wangnengjie <751614701@qq.com>
2022-07-23 13:06:08 +0800

    cache trait & lru

---
+++ src/shard.rs
@@ -0,0 +1,100 @@
+use std::{
+    collections::hash_map::RandomState,
+    hash::{BuildHasher, Hash},
+};
+
+use crate::{utils::hash_key, Cache, CacheHandle, CacheShard, Options};
+
+pub struct ShardCache<T: CacheShard, S = RandomState> {
+    shards: Vec<T>,
+    hash_builder: S,
+}
+
+impl<T> ShardCache<T, RandomState>
+where
+    T: CacheShard<HashBuilder = RandomState>,
+{
+    pub fn new(capacity: usize, shard_bits: usize) -> Self {
+        Self::with_hasher(capacity, shard_bits, RandomState::default())
+    }
+}
+
+impl<T, S> ShardCache<T, S>
+where
+    T: CacheShard<HashBuilder = S>,
+    S: Clone,
+{
+    pub fn with_hasher(capacity: usize, shard_bits: usize, hash_builder: S) -> Self {
+        let num_shards = 1 << shard_bits;
+        let per_shard = (capacity + (num_shards - 1)) / num_shards;
+        Self {
+            shards: (0..num_shards)
+                .map(|_| {
+                    T::shard_with_options(Options {
+                        capacity: per_shard,
+                        shard_bits: 0,
+                        hash_builder: hash_builder.clone(),
+                    })
+                })
+                .collect(),
+            hash_builder: hash_builder.clone(),
+        }
+    }
+}
+
+impl<T, S> Cache for ShardCache<T, S>
+where
+    T: CacheShard<HashBuilder = S>,
+    S: Send + Sync,
+{
+    type Key = T::Key;
+    type Value = T::Value;
+    type HashBuilder = S;
+
+    fn with_options(opts: Options<S>) -> Self
+    where
+        Self::HashBuilder: Clone,
+        Self: Sized,
+    {
+        Self::with_hasher(opts.capacity, opts.shard_bits, opts.hash_builder.clone())
+    }
+
+    fn insert(
+        &self,
+        key: Self::Key,
+        value: Self::Value,
+        charge: usize,
+    ) -> Box<dyn CacheHandle<Value = Self::Value> + '_>
+    where
+        Self::Key: Hash + Eq,
+        Self::HashBuilder: BuildHasher,
+    {
+        let hash = hash_key(&self.hash_builder, &key);
+        self.shards[hash as usize & self.shards.len() - 1].shard_insert(key, value, hash, charge)
+    }
+
+    fn lookup(&self, key: &Self::Key) -> Option<Box<dyn CacheHandle<Value = Self::Value> + '_>>
+    where
+        Self::Key: Hash + Eq,
+        Self::HashBuilder: BuildHasher,
+    {
+        let hash = hash_key(&self.hash_builder, &key);
+        self.shards[hash as usize & self.shards.len() - 1].shard_lookup(key, hash)
+    }
+
+    fn erase(&self, key: &Self::Key)
+    where
+        Self::Key: Hash + Eq,
+        Self::HashBuilder: BuildHasher,
+    {
+        let hash = hash_key(&self.hash_builder, &key);
+        self.shards[hash as usize & self.shards.len() - 1].shard_erase(key, hash)
+    }
+
+    fn prune(&self)
+    where
+        Self::Key: Eq,
+    {
+        self.shards.iter().for_each(|s| s.shard_prune())
+    }
+}
